ce933f2d4c65ca5c1a210aab63a411a0594e143e
Brent Jackson <jxnblk@github.com>
2015-02-10 10:45:41 -0500

    Edit tweet button copy

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ module.exports = React.createClass({
           </h1>
           <p className="m0">{this.props.description}</p>
         </div>
-        <a href="https://twitter.com/intent/tweet?text=Throw some shade&amp;url=http://jxnblk.com/shade&amp;via=jxnblk"
+        <a href="https://twitter.com/intent/tweet?text=Mathematically derived gradient explorer&amp;url=http://jxnblk.com/shade&amp;via=jxnblk"
           className="button button-nav-dark bg-darken-2 rounded">
             Tweet
         </a>
